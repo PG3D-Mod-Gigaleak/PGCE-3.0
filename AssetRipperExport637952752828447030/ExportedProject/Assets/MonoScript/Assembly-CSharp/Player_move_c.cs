68a1bc725ff1776765b4a399fd8fb11260486d4e
EnderPurrson <96633985+EnderPurrson@users.noreply.github.com>
2022-10-07 17:31:05 -0400

    ugghghuh
    fghfgh

--- AssetRipperExport637952752828447030/ExportedProject/Assets/MonoScript/Assembly-CSharp/Player_move_c.cs
+++ AssetRipperExport637952752828447030/ExportedProject/Assets/MonoScript/Assembly-CSharp/Player_move_c.cs
@@ -1962,12 +1962,15 @@ public sealed class Player_move_c : MonoBehaviour
 			if (id == gameObject.GetComponent<PhotonView>().viewID)
 			{
 				gameObject.GetComponent<Player_move_c>().GetVisibleWear(gameObject.GetComponent<Player_move_c>().curGear).SetActive(true);
+				Debug.LogError(gameObject.GetComponent<Player_move_c>().curGear);
 			}
 		}
 	}
 
 	private void Start()
 	{
+		curGear = PlayerPrefs.GetString("gear");
+		Debug.LogError(PlayerPrefs.GetString("gear"));
 		widthPoduct = (float)(healthInApp.normal.background.width * Screen.height) / 768f * (320f / (float)healthInApp.normal.background.height);
 		if (PlayerPrefs.GetInt("MultyPlayer") == 1)
 		{
@@ -2185,7 +2188,6 @@ public sealed class Player_move_c : MonoBehaviour
 			base.transform.parent.transform.rotation = GlobalGameController.rotMyPlayer;
 			PlayerPrefs.SetInt("StartAfterDisconnect", 0);
 		}
-		curGear = PlayerPrefs.GetString("gear");
 		if (PlayerPrefs.GetString("TypeConnect").Equals("local"))
 		{
 			base.GetComponent<NetworkView>().RPC("setVisibleWear", RPCMode.Others, base.gameObject.GetComponent<NetworkView>().viewID);
