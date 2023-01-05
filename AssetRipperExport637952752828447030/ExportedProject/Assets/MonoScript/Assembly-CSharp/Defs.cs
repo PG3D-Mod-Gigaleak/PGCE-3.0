dcc66f95c2ef0d81ed2fa4a62156e7a955a4b1e7
happybahn <110716488+happybahn@users.noreply.github.com>
2023-01-05 23:46:07 +0200

    ben
    update

--- AssetRipperExport637952752828447030/ExportedProject/Assets/MonoScript/Assembly-CSharp/Defs.cs
+++ AssetRipperExport637952752828447030/ExportedProject/Assets/MonoScript/Assembly-CSharp/Defs.cs
@@ -14,7 +14,7 @@ public sealed class Defs
 
 	public static string cat5;
 
-	public static int numOfWeapons = 121;
+	public static int numOfWeapons = 122;
 
 	public static SurvivalConfig survivalConfig;
 
--- AssetRipperExport637952752828447030/ExportedProject/Assets/MonoScript/Assembly-CSharp/WeaponManager.cs
+++ AssetRipperExport637952752828447030/ExportedProject/Assets/MonoScript/Assembly-CSharp/WeaponManager.cs
@@ -444,7 +444,7 @@ public sealed class WeaponManager : MonoBehaviour
 			"mech suit", "TigerPistol", "mp5", "Trophy", "buntu", "fishgun", "tibhategun", "sniper rifle", "drip hammer", "Hammer (3.6)", "unknown box", "sodamachine", "disability", "martian ray gun", "4jfyHJ6X93jtr", "piramid", "comzp90", "comzmp5", "comzak47", "comzaug",
 			"comzxm", "comzremington", "comzshotgun", "enditank", "zombie", "SVD", "chicken", "toast cube", "spaceship", "oxbadcode but normal", "abominationAK", "monstrosity", "helicopter", "spark shark", "killer bean's pistol", "batman's glove", "Mr. commit pixelman",
 			"fn20000", "pirate blunderbuss", "mp5andknife", "rust ak47", "Christmas bow from CNRPG1", "swfminigun1", "swfminigun2", "crappy chainsaw", "swfminigun3", "fake pixel gun", "Blackbeard's Pistols", "dualp90", "destructor", "dualzombiep90s", "zombie apocalypse destructor",
-			"spiderswords", "longinus", "neutron rifle", "bcw mustang"
+			"spiderswords", "longinus", "neutron rifle", "bcw mustang", "ben"
 		};
 		_initialMultiplayerWeaponTags = new string[]
 		{
