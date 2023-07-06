a3d4811960f790fea753ab3b3dd72f3c93e4a0ad
EnderPurrson <enderlord9990@gmail.com>
2023-07-06 07:45:55 -0400

    added scale >:)

--- Assets/MonoScript/Assembly-CSharp/NetworkInterpolatedTransform.cs
+++ Assets/MonoScript/Assembly-CSharp/NetworkInterpolatedTransform.cs
@@ -6,6 +6,8 @@ public class NetworkInterpolatedTransform : MonoBehaviour
 
 	private Quaternion correctPlayerRot = Quaternion.identity;
 
+	private Vector3 correctPlayerScale = Vector3.zero;
+
 	private void Awake()
 	{
 		if (PlayerPrefs.GetInt("MultyPlayer") != 1 || PlayerPrefs.GetString("TypeConnect").Equals("inet"))
@@ -20,17 +22,22 @@ public class NetworkInterpolatedTransform : MonoBehaviour
 		{
 			Vector3 value = base.transform.localPosition;
 			Quaternion value2 = base.transform.localRotation;
+			Vector3 value3 = base.transform.localScale;
 			stream.Serialize(ref value);
 			stream.Serialize(ref value2);
+			stream.Serialize(ref value3);
 		}
 		else
 		{
 			Vector3 value3 = Vector3.zero;
 			Quaternion value4 = Quaternion.identity;
+			Vector3 value5 = Vector3.zero;
 			stream.Serialize(ref value3);
 			stream.Serialize(ref value4);
+			stream.Serialize(ref value5);
 			correctPlayerPos = value3;
 			correctPlayerRot = value4;
+			correctPlayerScale = value5;
 		}
 	}
 
@@ -40,6 +47,7 @@ public class NetworkInterpolatedTransform : MonoBehaviour
 		{
 			base.transform.position = Vector3.Lerp(base.transform.position, correctPlayerPos, Time.deltaTime * 5f);
 			base.transform.rotation = Quaternion.Lerp(base.transform.rotation, correctPlayerRot, Time.deltaTime * 5f);
+			base.transform.localScale = Vector3.Lerp(base.transform.localScale, correctPlayerScale, Time.deltaTime * 5f);
 		}
 	}
 }
