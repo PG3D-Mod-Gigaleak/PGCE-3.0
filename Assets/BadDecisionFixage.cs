a1f03b34898f477b1a979b67c727282018d6044c
EnderPurrson <enderlord9990@gmail.com>
2023-07-09 12:54:35 -0400

    fixed all your goofy, nix!

---
+++ Assets/BadDecisionFixage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BadDecisionFixage : Editor
+{
+	[MenuItem("Bad Decision Fixage/Fix Nix's Goofy M16 Workarounds")]
+	public static void FixNixsGoofysharedMaterials()
+	{
+		foreach (GameObject weapon in Resources.LoadAll<GameObject>("weapons"))
+		{
+			foreach (MeshRenderer renderer in weapon.GetComponentsInChildren<MeshRenderer>())
+			{
+				if (renderer == null || renderer.sharedMaterial == null)
+				{
+					Debug.LogError("null lol");
+					continue;
+				}
+				if (renderer.sharedMaterial.name == "test" || renderer.sharedMaterial.name == "test 1")
+				{
+					renderer.enabled = false;
+				}
+			}
+		}
+	}
+}
