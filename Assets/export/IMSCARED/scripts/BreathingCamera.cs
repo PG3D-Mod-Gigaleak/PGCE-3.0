d7163224e60dc64baec810dbe09ac96d39bade4e
zUltima <ggyuuhallo233@proton.me>
2023-07-25 11:12:42 +0200

    funny imscared camera breathe script

---
+++ Assets/export/IMSCARED/scripts/BreathingCamera.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IMSCARED {
+	public class BreathingCamera : MonoBehaviour {
+		void Update() {
+			transform.localPosition = new Vector3(0, Mathf.Sin(Time.time*0.45f)/8.5f, 0);
+		}
+	}
+}
