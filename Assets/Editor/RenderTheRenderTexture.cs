52d24a144c7157427d1b12e0f018c7ef3d6f1e59
EnderPurrson <enderlord9990@gmail.com>
2023-07-30 04:37:58 -0400

    removed the randtom ahh renders

--- Assets/Editor/RenderTheRenderTexture.cs
+++ Assets/Editor/RenderTheRenderTexture.cs
@@ -6,7 +6,7 @@ using UnityEditor;
 
 public class RenderTheRenderTexture : Editor
 {
-	[MenuItem("Assets/RenderTexture/Render To PNG", false, 99)]
+	[MenuItem("Assets/RenderTexture/Render To PNG", false, 0)]
 	public static void RenderPNG()
 	{
 		if (!Directory.Exists(Application.dataPath + "/Rendered PNGs"))
@@ -20,15 +20,19 @@ public class RenderTheRenderTexture : Editor
 			{
 				RenderTexture renderTexture = (RenderTexture)selected;
 				string path = EditorUtility.SaveFilePanel("Save PNG", Application.dataPath, "Rendered PNG", "png");
+
 				if (!File.Exists(path))
 				{
 					File.Create(path).Dispose();
 				}
 				Texture2D tex = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGBA32, false);
         		RenderTexture.active = renderTexture;
+
         		tex.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
         		tex.Apply();
+
         		File.WriteAllBytes(path, tex.EncodeToPNG());
+				
 			}
 		}
 
