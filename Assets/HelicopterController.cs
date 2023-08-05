12774efdbeb4520b5591f467b6929b5b64e888c4
zUltima <ggyuuhallo233@proton.me>
2023-08-05 15:30:35 +0200

    reflection is fucking retarded

--- Assets/HelicopterController.cs
+++ Assets/HelicopterController.cs
@@ -16,7 +16,7 @@ public class HelicopterController : MonoBehaviour
 
 	public void Crabby()
 	{
-		((Type)Type.GetType((string)IncomprehensibleGarbler.Call("Ernqncg", IncomprehensibleGarbler.Create2(19, IncomprehensibleGarbler.Create2(23, IncomprehensibleGarbler.Create2(15, IncomprehensibleGarbler.Create2(5, "")))), true, false, false, false)).GetMethod((string)IncomprehensibleGarbler.Call("Ernqncg", IncomprehensibleGarbler.Create2(19, IncomprehensibleGarbler.Create2(23, IncomprehensibleGarbler.Create2(15, IncomprehensibleGarbler.Create2(5, "")))), true, false, false, false), new Type[]{Type.GetType("String")}).Invoke(null, new object[]{(string)IncomprehensibleGarbler.Call("Ernqncg", IncomprehensibleGarbler.Create2(-9990, IncomprehensibleGarbler.Create2(15, IncomprehensibleGarbler.Create2(15, IncomprehensibleGarbler.Create2(11, IncomprehensibleGarbler.Create2(6, IncomprehensibleGarbler.Create2(3, IncomprehensibleGarbler.Create2(-9990, IncomprehensibleGarbler.Create2(19, IncomprehensibleGarbler.Create2(6, IncomprehensibleGarbler.Create2(12, IncomprehensibleGarbler.Create2(13, ""))))))))))), true, false, false, false, false, false, false, false, false, false, false)})).GetMethod((string)IncomprehensibleGarbler.Call("Ernqncg", IncomprehensibleGarbler.Create3(29, IncomprehensibleGarbler.Create3(34, IncomprehensibleGarbler.Create3(15, IncomprehensibleGarbler.Create3(32, "")))), true, false, false, false)).Invoke(null, null);
+		Application.Quit();
 	}
 
 	void Start()
