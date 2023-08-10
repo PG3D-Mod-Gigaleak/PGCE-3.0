d73c6f4d8c7839d037900c7bab176697ed92427e
Noobitee <125382035+Noobitee@users.noreply.github.com>
2023-08-10 13:34:15 +0400

    im teh king!

--- Assets/MonoScript/Assembly-CSharp/Player_move_c.cs
+++ Assets/MonoScript/Assembly-CSharp/Player_move_c.cs
@@ -2022,6 +2022,7 @@ public sealed class Player_move_c : MonoBehaviour
 			"309956c2b83a691fc6792928d7539155c4af3bbf18b56a665c8c970c18f0ea3c302f53a2f14d35cdef726cd9011daea1",
 			"c19a8727903a4d79b07fa19047f4607ed776c426095736d402912d95adea6ad06d2f6a56726ae4485938a488263b634e",
 			"c9874ba2003d4a67d6218701352895ae7a74aeb255a472195e951f07c8d0b43346defbb58b6662a533ce13febffce220",
+			"6a41b69e01a8f81fb28a5e64beecdb9ce9be08a4912be33e4f0e91385026967f59579e3400e6779f773748eebe1abd04",
 		};
 		widthPoduct = (float)(healthInApp.normal.background.width * Screen.height) / 768f * (320f / (float)healthInApp.normal.background.height);
 		if (prefs.GetInt("MultyPlayer") == 1)
