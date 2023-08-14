2c7076d204e535fe4208499004252792bcc08d8e
zUltima <ggyuuhallo233@proton.me>
2023-08-14 16:32:29 +0200

    the shaky shaky

---
+++ Assets/ShakyNGUIPanel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakyNGUIPanel : MonoBehaviour
+{
+    void Update()
+    {
+        transform.localPosition = new Vector3(Random.Range(-2, 2), Random.Range(-2, 2), 0);
+        transform.localRotation = Quaternion.Euler(0, 0, Random.Range(-2, 2));
+    }
+}
