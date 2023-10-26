8e9c9c08bea482f54f8d76a63b8dca15310c34c9
Noobite <125382035+Noobitee@users.noreply.github.com>
2023-10-26 13:40:31 +0400

    spawnage

---
+++ Assets/Weapon/Idk/SpawnCubes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class SpawnCubes : MonoBehaviour
+{
+    public GameObject cuberedPrefab;
+    public UnityEvent onSpawn;
+    
+    void Update() {
+        Debug.LogError("Spawned");
+        Instantiate(cuberedPrefab, transform.position, Quaternion.identity);
+        onSpawn?.Invoke();
+    }
+}
