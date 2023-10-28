8fe6deb1b71e57c7dbb2b3bd868c8de08dae53ae
Noobite <125382035+Noobitee@users.noreply.github.com>
2023-10-29 00:29:03 +0400

    idk

---
+++ Assets/Sourсe/Scenes/CursedGang/Teleport1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport1 : MonoBehaviour
+{
+    private void onTriggerEnter(Collider other) 
+    {
+
+        other.transform.position = new Vector3(-50f,0f,0f);
+        Debug.LogError("Teleported");
+    }
+}
