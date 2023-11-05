e395b0f670871e701589556cc49d5e16ddd77708
Noobite <125382035+Noobitee@users.noreply.github.com>
2023-11-05 11:04:29 +0400

    update all enemies

--- Assets/Weapon/Idk/HealthNPC.cs
+++ Assets/Weapon/Idk/HealthNPC.cs
@@ -10,25 +10,35 @@ public class HealthNPC : MonoBehaviour
     public Transform targetNPC;
     private float value;
     public Transform fillBar;
+    private int alreadyPlayed = 0;
+    private float timer = 1f;
+    private float animLength = 1f;
     void Awake()
     {
         instance = this;
     }
     void Update()
     {
-        try
+        float health = targetNPC.GetComponent<ZombiUpravlenie>().health;
+        float maxhealth = targetNPC.GetChild(0).GetComponent<Sounds>().health;
+        if (health <= 0f)
         {
-            targetPos = new Vector3(targetNPC.position.x,(targetNPC.position.y + 3), targetNPC.position.z);
-        }
-        catch (MissingReferenceException) // Check if targetNPC is there
-        {
-            Destroy(gameObject);
+            animLength = gameObject.GetComponent<Animation>()["HealthDown"].length;
+            if (alreadyPlayed == 0)
+            {
+                gameObject.GetComponent<Animation>().Play("HealthDown");
+                timer = animLength;
+                alreadyPlayed = 1;
+            }
+            timer -= Time.deltaTime;
+            if (timer < 0f)
+            {
+                Destroy(gameObject);
+            }
         }
         targetPos = new Vector3(targetNPC.position.x,(targetNPC.position.y + 3), targetNPC.position.z);
         if (targetNPC != null)
         {
-            float health = targetNPC.GetComponent<ZombiUpravlenie>().health;
-            float maxhealth = targetNPC.GetChild(0).GetComponent<Sounds>().health;
             if (health < 0f)
             {
                 health = 0f;
@@ -38,7 +48,7 @@ public class HealthNPC : MonoBehaviour
             {
                 value = health / maxhealth;
             }
-            fillBar.localScale = new Vector3(Mathf.Lerp(fillBar.localScale.x, value, Time.deltaTime * 3f), fillBar.localScale.y, fillBar.localScale.z);
+            fillBar.localScale = new Vector3(Mathf.Lerp(fillBar.localScale.x, value, Time.deltaTime * 15f), fillBar.localScale.y, fillBar.localScale.z);
         }
         transform.LookAt(Camera.main.transform.position);
         transform.localPosition = new Vector3(targetPos.x,targetPos.y,targetPos.z);
