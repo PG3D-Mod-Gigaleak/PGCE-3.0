b0f0f46662c340b5c68748aa55e7a93ad3ce06da
EnderPurrson <enderlord9990@gmail.com>
2023-11-07 14:21:00 -0500

    lighting fixage

---
+++ Assets/Editor/FindTheDiffuse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+public class FindTheDiffuse : Editor
+{
+    [MenuItem("Fixage/Find The Diffuse")]
+    public static void DiffuseTheDiffuse()
+    {
+        Selection.objects = (from guid in AssetDatabase.FindAssets("t:Material") where AssetDatabase.LoadAssetAtPath<Material>(AssetDatabase.GUIDToAssetPath(guid)).shader.name == "Mobile/Diffuse" select AssetDatabase.LoadAssetAtPath<Material>(AssetDatabase.GUIDToAssetPath(guid))).ToArray();
+    }
+}
