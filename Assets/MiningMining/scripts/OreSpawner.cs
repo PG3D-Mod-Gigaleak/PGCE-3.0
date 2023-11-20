4c4142456fc990e941317cd77ba91d13b7be2d79
Noobite <125382035+Noobitee@users.noreply.github.com>
2023-11-20 20:12:17 +0400

    astral infection + new boss

--- Assets/MiningMining/scripts/OreSpawner.cs
+++ Assets/MiningMining/scripts/OreSpawner.cs
@@ -10,6 +10,22 @@ public class OreSpawner : MonoBehaviour
 
     private int currentOres;
 
+    public float AstralChance;
+
+    public Color AstralColor;
+
+    public float newDensity;
+
+    public AudioClip AstralMusic;
+
+    private AudioSource bgm;
+
+    public Material AstralSkybox;
+
+    public GameObject AstralOre;
+
+    public bool isAstral = false;
+
     public float GetRandX(Mineable ore)
     {
         //return UnityEngine.Random.Range(0, 2) == 1 ? UnityEngine.Random.Range(ore.boundsX.x, ore.boundsX.y) : -UnityEngine.Random.Range(ore.boundsX.x, ore.boundsX.y);
@@ -22,31 +38,68 @@ public class OreSpawner : MonoBehaviour
         return UnityEngine.Random.Range(-1000, 1000);
     }
 
+    void Start()
+    {
+        bgm = GameObject.Find("BGM").GetComponent<AudioSource>();
+        AstralChance *= 1000;
+    }
     void Update()
     {
         currentOres = 0;
+        float Roll = Random.Range(1f,1000000f);
+        if (Roll < AstralChance)
+		{
+            if (isAstral == false)
+            {
+                GameObject[] array2 = GameObject.FindGameObjectsWithTag("Ore");
+                foreach (GameObject gameObject in array2)
+                {
+                    Destroy(gameObject);
+                }
+                GameObject.Find("Plane").GetComponent<Animation>().Play("ToAstral");
+                bgm.gameObject.GetComponent<MusicController>().enabled = !bgm.gameObject.GetComponent<MusicController>().enabled;
+                bgm.Stop();
+                bgm.clip = AstralMusic;
+                bgm.volume = 1f;
+                bgm.Play();
+                RenderSettings.skybox = AstralSkybox;
+	            RenderSettings.ambientLight = AstralColor;
+                RenderSettings.fogColor = AstralColor;
+                RenderSettings.fogDensity = newDensity;
+                isAstral = true;
+            }
+        }
         GameObject[] array = GameObject.FindGameObjectsWithTag("Ore");
-        foreach (var GameObject in array)
+        foreach (GameObject gameObject in array)
         {
             currentOres += 1;
         }
-        if (currentOres >= 1000)
+        if (currentOres >= 2000)
         {
             return;
         }
         float rand = UnityEngine.Random.Range(0f, 100.01f);
-        foreach (Mineable anOre in ores)
+        if (isAstral == false)
         {
-            if (rand <= anOre.randomRange.y && rand >= anOre.randomRange.x)
+            foreach (Mineable anOre in ores)
             {
-                Instantiate(anOre.gameObject, new Vector3(GetRandX(anOre), -4f, GetRandY(anOre)), Quaternion.identity);
-                break;
+                if (rand <= anOre.randomRange.y && rand >= anOre.randomRange.x)
+                {
+                    Instantiate(anOre.gameObject, new Vector3(GetRandX(anOre), -4f, GetRandY(anOre)), Quaternion.identity);
+                    break;
+                }
+                else if (rand > 93.55)
+                {
+                    Instantiate(ores[0].gameObject, new Vector3(GetRandX(ores[0]), -4f, GetRandY(ores[0])), Quaternion.identity);
+                    break;
+                }
             }
-            else if (rand > 93.55)
-            {
-                Instantiate(ores[0].gameObject, new Vector3(GetRandX(ores[0]), -4f, GetRandY(ores[0])), Quaternion.identity);
-                break;
-            }
+        }
+        else if (isAstral == true)
+        {
+            float randx = Random.Range(-1000f, 1000f);
+            float randz = Random.Range(-1000f, 1000f);
+            Instantiate(AstralOre, new Vector3(randx, -4f, randz), Quaternion.identity);
         }
     }
 }
