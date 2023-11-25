42b91cd22055caa925529970d97db76483057f86
Noobite <125382035+Noobitee@users.noreply.github.com>
2023-11-25 23:36:23 +0400

    sniper locking

---
+++ Assets/Imports/BattleBears/Weapons/Deadeye/DamageTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using UnityEngine;
+
+public class DamageTarget : MonoBehaviour
+{
+    public void CallDamageZone()
+    {
+        if (gameObject.transform.GetChild(0).GetChild(0).GetComponent<LockDetect>().hasTarget == true)
+        {
+            gameObject.transform.GetChild(0).GetChild(0).GetComponent<DamageZone>().DamageTarget();
+        }
+    }
+    public void CallDamageCall()
+    {
+        gameObject.transform.GetChild(0).GetChild(0).GetComponent<LockDetect>().DamageCall();
+    }
+}
