4ad2271261aae6accb115c320b8cd02afb178d01
Noobite <125382035+Noobitee@users.noreply.github.com>
2023-12-06 03:04:01 +0400

    portal nix

---
+++ Assets/Weapon/Idk/Portal Travel/NixShoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NixShoot : MonoBehaviour
+{
+    public Transform targetObject;
+    public GameObject bullet;
+    public float Distance;
+    public Transform ShootPos;
+
+    void Start()
+    {
+        ShootPos = transform.Find("ShootPos");
+    }
+    void Shoot()
+    {
+        Distance = Vector3.Distance(ShootPos.position,targetObject.transform.position - new Vector3(0,0f,0));
+        GameObject bulletShoot = Instantiate(bullet, ShootPos.position, ShootPos.rotation);
+        bulletShoot.transform.LookAt(targetObject.position - new Vector3(0,0f,0));
+        bulletShoot.transform.GetChild(0).localScale = new Vector3(bulletShoot.transform.GetChild(0).localScale.x, bulletShoot.transform.GetChild(0).localScale.y * Distance, bulletShoot.transform.GetChild(0).localScale.z);
+        bulletShoot.transform.position += bulletShoot.transform.forward * ( Distance / 2);
+    }
+}
---
+++ Assets/Weapon/Idk/Portal Travel/PortalTravel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTravel : MonoBehaviour
+{
+    public GameObject Nix;
+    public Transform TargetObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TargetObject = gameObject.transform;
+        for(int i = 0; i < 6; i++)
+        {
+            float x = Random.Range(-10f,10f);
+            float y = Random.Range(2,10f);
+            float z = Random.Range(-10f,10f);
+            Vector3 randposition = new Vector3(x,y,z);
+            GameObject NixSpawn = Instantiate(Nix, TargetObject.position, TargetObject.rotation);
+            NixSpawn.transform.position += randposition;
+            NixSpawn.GetComponent<NixShoot>().targetObject = TargetObject;
+            NixSpawn.transform.LookAt(NixSpawn.GetComponent<NixShoot>().targetObject.position);
+        }
+    }
+}
