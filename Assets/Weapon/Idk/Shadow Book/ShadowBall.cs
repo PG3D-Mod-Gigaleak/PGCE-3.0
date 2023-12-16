aee8a38b98b1c73a91a0d894af36995837807aa5
Noobite <125382035+Noobitee@users.noreply.github.com>
2023-12-16 18:18:01 +0400

    mage class part 2

---
+++ Assets/Weapon/Idk/Shadow Book/ShadowBall.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowBall : MonoBehaviour
+{
+    public bool Discharge = false;
+    public bool AlreadyDischarged = false;
+    public float OrbCooldown = 1f;
+    public GameObject ShadowOrb;
+    public float detectRadius = 0f;
+    public Transform Target;
+    void Update()
+    {
+        if (Discharge == true && AlreadyDischarged == false)
+        {
+            AlreadyDischarged = true;
+            Discharg();
+        }
+        if (transform.localScale.x < 0f && transform.localScale.y < 0f && transform.localScale.z < 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (Discharge == false && AlreadyDischarged == false)
+        {
+            transform.localScale = new Vector3(transform.localScale.x + 0.02f, transform.localScale.y + 0.02f, transform.localScale.z + 0.02f);
+            detectRadius += 0.2f;
+        }
+    }
+
+    void Discharg()
+    {
+        StartCoroutine(Dis());
+    }
+
+    public IEnumerator Dis()
+    {
+        float num = detectRadius;
+        Target = null;
+        GameObject[] array = GameObject.FindGameObjectsWithTag("ZombieCollider");
+        foreach (GameObject gameObject in array)
+        {
+            if (Vector3.Distance(base.transform.position,gameObject.transform.position) < num && gameObject.transform.parent.GetComponent<ZombiUpravlenie>().health > 0f)
+            {
+                Target = gameObject.transform;
+                num = Vector3.Distance(base.transform.position,gameObject.transform.position);
+            }
+        }
+        GameObject[] array2 = GameObject.FindGameObjectsWithTag("BodyCollider");
+        // foreach (GameObject gameObject in array2)
+        // {
+        //     if (Vector3.Distance(base.transform.position,gameObject.transform.position) < num && gameObject.GetComponent<FirstPersonControl>().isMine == false)
+        //     {
+        //         Target = gameObject.transform;
+        //         num = Vector3.Distance(base.transform.position,gameObject.transform.position);
+        //     }
+        // }
+        if (Target != null)
+        {
+            GameObject SpawnedOrb = Instantiate(ShadowOrb, transform.position, Quaternion.identity);
+            SpawnedOrb.GetComponent<Rigidbody>().velocity = new Vector3(0f,0f,0f);
+            SpawnedOrb.transform.LookAt(Target.position);
+            SpawnedOrb.GetComponent<Rigidbody>().AddForce(SpawnedOrb.transform.forward * 1500);
+        }
+        transform.localScale = new Vector3(transform.localScale.x - 0.02f,transform.localScale.y - 0.02f,transform.localScale.z - 0.02f);
+        detectRadius -= 0.2f;
+        yield return new WaitForSeconds(OrbCooldown);
+        Discharg();
+    }
+}
---
+++ Assets/Weapon/Idk/Shadow Book/ShadowBook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowBook : MonoBehaviour
+{
+    public bool isAlreadySpawned = false;
+    public GameObject ShadowBall;
+    public GameObject SpawnedBall;
+    public float OrbCooldown;
+    public GameObject host;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && isAlreadySpawned == false)
+        {
+            SpawnedBall = Instantiate(ShadowBall, transform.position + (transform.forward * 10) + new Vector3(0f,5f,0f), Quaternion.identity);
+            SpawnedBall.GetComponent<DespawnObject>().hostObject = host;
+            SpawnedBall.GetComponent<ShadowBall>().OrbCooldown = OrbCooldown;
+            SpawnedBall.GetComponent<ShadowBall>().Discharge = false;
+            isAlreadySpawned = true;
+        }
+        if (Input.GetMouseButtonUp(0) && isAlreadySpawned == true)
+        {
+            SpawnedBall.GetComponent<ShadowBall>().Discharge = true;
+            isAlreadySpawned = false;
+        }
+    }
+}
