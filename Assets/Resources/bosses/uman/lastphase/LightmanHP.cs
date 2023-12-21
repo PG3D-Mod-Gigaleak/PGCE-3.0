86cbdbebb524f554d9d61efc71c52228b2577093
Noobite <125382035+Noobitee@users.noreply.github.com>
2023-12-21 16:27:32 +0400

    boss24

---
+++ Assets/Resources/bosses/uman/lastphase/LightmanHP.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightmanHP : MonoBehaviour
+{
+    public AudioSource bgmusic;
+    public AudioClip music;
+    void Start()
+    {
+        bgmusic = GameObject.FindGameObjectWithTag("BGM").GetComponent<AudioSource>();
+        bgmusic.Stop();
+        bgmusic.clip = music;
+        bgmusic.Play();
+
+    }
+    void FixedUpdate()
+    {
+        gameObject.GetComponent<ZombiUpravlenie>().health -= 100f;
+        if (gameObject.GetComponent<ZombiUpravlenie>().health <= 0f)
+        {
+            bgmusic.Stop();
+        }
+    }
+}
