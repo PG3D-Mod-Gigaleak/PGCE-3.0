67b7a7b8c3c0ca5e2a5adafd48fa1311f6fff68d
Noobite <125382035+Noobitee@users.noreply.github.com>
2023-12-25 16:13:47 +0400

    knife party ( not melee only yet )

---
+++ Assets/Scene/Sources/Scenes/Knife/KnifeOnly.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeOnly : MonoBehaviour
+{
+    public WeaponManager wm;
+
+    void Start()
+    {
+        wm = GameObject.FindGameObjectWithTag("WeaponManager").GetComponent<WeaponManager>();
+    }
+    void Update()
+    {
+    }
+}
