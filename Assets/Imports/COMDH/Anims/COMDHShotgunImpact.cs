418d485e52da3eaeedfad3b4b26b0e319a84648f
Noobite <125382035+Noobitee@users.noreply.github.com>
2024-01-01 03:29:12 +0400

    comdh

---
+++ Assets/Imports/COMDH/Anims/COMDHShotgunImpact.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class COMDHShotgunImpact : MonoBehaviour
+{
+    public GameObject effect;
+    public GameObject host;
+    public GameObject player;
+    public bool ImpactNow;
+
+    public List<GameObject> array = new List<GameObject>();
+
+    void Start()
+    {
+        player = host.transform.parent.parent.parent.gameObject;
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (ImpactNow == true && !array.Contains(other.gameObject))
+        {
+            if (other.tag == "BodyCollider")
+            {
+                if (other.transform.parent.gameObject != player)
+                {
+                    Instantiate(effect, other.transform.position + new Vector3(0,1,0), Quaternion.identity);
+                    array.Add(other.gameObject);
+                }
+            }
+            else if (other.tag == "ZombieCollider")
+            {
+                Instantiate(effect, other.transform.position + new Vector3(0,1f,0), Quaternion.identity);
+                array.Add(other.gameObject);
+            }
+        }
+    }
+}
