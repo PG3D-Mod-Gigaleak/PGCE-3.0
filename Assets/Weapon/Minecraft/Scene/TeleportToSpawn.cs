e0e45e1021a1913bec877e1344d828466b29435f
Noobite <125382035+Noobitee@users.noreply.github.com>
2024-04-05 17:38:38 +0400

    improved building place

---
+++ Assets/Weapon/Minecraft/Scene/TeleportToSpawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportToSpawn : MonoBehaviour
+{
+    public string objecttarget;
+    public GameObject spawnbox;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == objecttarget)
+        {
+            other.transform.position = spawnbox.transform.position;
+        }
+    }
+}
